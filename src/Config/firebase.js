1a5ff2e57f8e9390819ad94445ef5263983ee822
DanStew <danielstewarts2004@gmail.com>
2024-09-04 14:58:55 +0100

    Setting up the initial React and Firebase

---
+++ src/Config/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAaAAjFKm6D5955o-joqRPv6RczfNHA5ws",
+  authDomain: "portfolio-website-ba654.firebaseapp.com",
+  projectId: "portfolio-website-ba654",
+  storageBucket: "portfolio-website-ba654.appspot.com",
+  messagingSenderId: "105012697171",
+  appId: "1:105012697171:web:f944d6d8e8712fca6f8ea6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
