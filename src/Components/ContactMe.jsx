5b9e8443a31f8e28a32fdede32e11b6792b86a3a
DanStew <danielstewarts2004@gmail.com>
2024-09-22 23:02:59 +0100

    Creating the design for the Contact Me page and most of the footer

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from "react";
 import githubLogo from "../Images/githubLogo.jpg";
 import linkedInLogo from "../Images/linkedInLogo.jpg";
-import emailjs from '@emailjs/browser'
+import emailjs from "@emailjs/browser";
 
 function ContactMe() {
   //Usestate variables to stores the inputs
@@ -10,70 +10,93 @@ function ContactMe() {
   const [email, setEmail] = useState("");
   const [title, setTitle] = useState("");
   const [message, setMessage] = useState("");
-  
+
   //Code to store success / error message
-  const [errorMsg,setErrorMsg] = useState("")
+  const [errorMsg, setErrorMsg] = useState("");
 
   //Useeffect to initalise emailjs with my public key
   useEffect(() => {
-    emailjs.init("iRAIRQra4UvUAAWb1")
-  },[])
+    emailjs.init("iRAIRQra4UvUAAWb1");
+  }, []);
 
   //Function to send me an email, using the form
   function sendEmail() {
     //Resetting the error message
-    setErrorMsg("")
+    setErrorMsg("");
     //Validating any inputs
-    if (name == "" || email == "" || title == "" || message == ""){
-      setErrorMsg("One or more email inputs are empty...")
-      return
+    if (name == "" || email == "" || title == "" || message == "") {
+      setErrorMsg("One or more email inputs are empty...");
+      return;
     }
 
     //Sending the email
-    emailjs.send("service_jp7hzwl","template_4ftmvv4",{
-      from_name: name,
-      email_id: email,
-      title: title,
-      message: message,
-    }).then(
-      (response) => {
-        //Noting success
-        setErrorMsg("Email Successfully Sent")
-        //Resetting values
-        setName("")
-        setEmail("")
-        setTitle("")
-        setMessage("")
-      },
-      (error) => {
-        console.log('FAILED...', error);
-        setErrorMsg("An error has occurred when sending the email")
-      },
-    );
+    emailjs
+      .send("service_jp7hzwl", "template_4ftmvv4", {
+        from_name: name,
+        email_id: email,
+        title: title,
+        message: message,
+      })
+      .then(
+        (response) => {
+          //Noting success
+          setErrorMsg("Email Successfully Sent");
+          //Resetting values
+          setName("");
+          setEmail("");
+          setTitle("");
+          setMessage("");
+        },
+        (error) => {
+          console.log("FAILED...", error);
+          setErrorMsg("An error has occurred when sending the email");
+        }
+      );
   }
 
   return (
     <div id="ContactMe" className="screen">
-      <p className="heading">ContactMe</p>
-      <div className="contactLeft">
-        <p className="boldRegular">Email : </p>
-        <p className="regular">danielstewarts2004@gmail.com</p>
-        <p className="boldRegular">Socials : </p>
-        <div className="socialsLine">
-          <img src={linkedInLogo} alt="Linked In Logo and Link" />
-          <img src={githubLogo} alt="Github Logo and Link" />
+      <p className="heading flexItems">Contact Me</p>
+      <div className="contactArea flexItems">
+        <div className="contactLeft flexItems">
+          <p className="heading">Email : </p>
+          <p className="subheading">danielstewarts2004@gmail.com</p>
+          <p className="heading">Socials : </p>
+          <div className="socialsLine">
+            <img src={linkedInLogo} onClick={() => window.open("https://github.com/")} alt="Linked In Logo and Link" />
+            <img src={githubLogo} onClick={() => window.open("https://www.linkedin.com/in/daniel-stewart-025107257/")} alt="Github Logo and Link" />
+          </div>
         </div>
-      </div>
-      <div className="contactRight">
-        <p className="subheading">Send an Email</p>
-        <form action="#">
-          <input onChange={(e) => setName(e.target.value)} value={name} type="text" placeholder="Enter your Name..."/>
-          <input onChange={(e) => setEmail(e.target.value)} value={email} type="email" placeholder="Enter your Email..."/>
-          <input onChange={(e) => setTitle(e.target.value)} value={title} type="text" placeholder="Enter the Email Title..."/>
-          <input onChange={(e) => setMessage(e.target.value)} value={message} type="text" placeholder="Enter your Message..."/>
-          <button onClick={() => sendEmail()} type="button">Send Email</button>
-        </form>
-        {errorMsg != "" ? <p className="error boldRegular">{errorMsg}</p> : <div style={{display:"none"}}></div>}
+        <div className="contactRight">
+          <p className="heading">Send an Email</p>
+          <form action="#">
+            <input
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              type="text"
+              placeholder="Enter your Name..."
+            />
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder="Enter your Email..."
+            />
+            <input
+              onChange={(e) => setTitle(e.target.value)}
+              value={title}
+              type="text"
+              placeholder="Enter the Email Title..."
+            />
+            <textarea rows="4" onChange={(e) => setMessage(e.target.value)} value={message} placeholder="Enter your Message..."></textarea>
+            <button onClick={() => sendEmail()} type="button">Send Email</button>
+          </form>
+          {errorMsg != "" ? (
+            <p className="error boldRegular">{errorMsg}</p>
+          ) : (
+            <div style={{ display: "none" }}></div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,16 @@
+import myCV from "../DanielStewartCV.pdf"
+import githubLogo from "../Images/githubLogo.jpg"
+import linkedInLogo from "../Images/linkedInLogo.jpg"
+
 function Footer(){
     return(
         <div className="Footer banner">
-            Footer
+            <p className="subheading">Designed and Built by Daniel Stewart</p>
+            <div className="footerLinks">
+                <a href={myCV} target="_blank"><button><span className="material-symbols-outlined">file_open</span><p className="subheading">Download My CV</p></button></a>
+                <img onClick={() => window.open("https://github.com/")}src={githubLogo} alt="Open my Github" />
+                <img onClick={() => window.open("https://www.linkedin.com/in/daniel-stewart-025107257/")}src={linkedInLogo} alt="Open my Linked In" />
+            </div>
         </div>
     )
 }
